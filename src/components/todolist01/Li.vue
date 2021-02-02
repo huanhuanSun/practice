4f9bad3d635f1ac355a68ef090004696b78cc6ce
huanhuan_sun <183668727@qq.com>
2021-02-02 15:30:03 +0800

    完善todolist

---
+++ src/components/todolist01/Li.vue
@@ -0,0 +1,40 @@
+<template>
+    <li>
+        <input 
+            type="checkbox" 
+            v-model="isChecked"
+        />
+        <p>{{todo.content}}</p>
+        <a @click="remove">-</a>
+    </li>
+</template>
+
+<script>
+    export default {
+        props:{
+            todo:Object,
+            toggleDone:Function,
+            removeTodo:Function
+        },
+        methods: {
+            remove(){
+                this.removeTodo(this.todo.id)
+            }
+        },
+        computed: {
+            isChecked:{
+                get(){
+                    return this.todo.done
+                },
+                set(){
+                    this.toggleDone(this.todo)
+                }
+            }
+        }
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
